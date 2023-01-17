6fddd91b6d759a410e9a83317e51e6b350b6a52b
bapt2 <5711@holbertonstudents.com>
2023-01-17 06:41:44 -0800

    fixed indent

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for c in range(10):
     for o in range(10):
         if c < o:
             if c == 8 and o == 9:
-                    print("{:d}{:d}".format(c, o))
-        else:
-                    print("{:d}{:d}".format(c, o), end=", ")
+                print("{:d}{:d}".format(c, o))
+            else:
+                print("{:d}{:d}".format(c, o), end=", ")
