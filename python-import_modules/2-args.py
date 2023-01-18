f194f9681b449c02111953e7edba15b0c3c9c07d
bapt2 <5711@holbertonstudents.com>
2023-01-18 10:47:40 -0800

    fixed PEP8

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-        import sys
-        for i in range(0, len(sys.argv)):
-                i += 1
-                i = i - 1
-        if i == 1:
-                print("{} argument:".format(i))
-        elif i == 0:
-                print("{} argument:".format(i))
-        else:
-                print("{} argument:".format(i))
-        for x in range(0, len(sys.argv)):
-                if x != 0:
-                        print("{}: {}".format(x, sys.argv[x]))
+import sys
+for i in range(0, len(sys.argv)):
+        i += 1
+        i = i - 1
+if i == 1:
+        print("{} arguments:".format(i))
+elif i == 0:
+        print("{} arguments:".format(i))
+else:
+        print("{} arguments:".format(i))
+for x in range(0, len(sys.argv)):
+        if x != 0:
+                print("{}: {}".format(x, sys.argv[x]))
