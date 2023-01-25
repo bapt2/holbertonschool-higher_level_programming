d4e51cd264ed5b33130bc2d503bf94dcbbd9caf2
bapt2 <5711@holbertonstudents.com>
2023-01-25 01:41:00 -0800

    fixed pep8

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
    n_m = []
    for i in range(len(matrix)):
-         n_m.append(list(map((lambda x: x ** 2), matrix[i])))
+      n_m.append(list(map((lambda x: x ** 2), matrix[i])))
    return n_m
