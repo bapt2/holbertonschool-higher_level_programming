bb0678c03be1e59e263aa69fbfc8425ab1131e75
bapt2 <5711@holbertonstudents.com>
2023-01-26 10:12:59 -0800

    try case where x can be bigger than the length of my_list

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,8 +3,8 @@ def safe_print_list(my_list=[], x=0):
     pos = 0
     try:
         for i in range(x):
+            print("{}".format(my_list[pos]), end="")
             pos += 1
-            print("{}".format(pos), end="")
         print()
         return pos
     except:
