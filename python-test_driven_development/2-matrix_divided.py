a91d2e34e4b68f097dc401cf43df128ece62b67e
bapt2 <5711@holbertonstudents.com>
2023-02-01 02:34:14 -0800

    add a function that divides all elements of a matrix

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" divide a matrix """
+
+
+def matrix_divided(matrix, div):
+    """ divide a matrix """
+    n_m = []
+    for i in range(len(matrix)):
+        if type(matrix) is not int:
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        if type(div) is not int or type(div) is not float:
+            raise TypeError("div must be a number")
+        if div == 0:
+            raise ZeroDivisionError("division by zero")
+
+            n_m.append(list(map((lambda x: round(x / div, 2)), matrix[i])))
+    return n_m
