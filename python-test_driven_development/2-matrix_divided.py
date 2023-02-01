7f1194251897d9c92591f89a34c8c06594d603a0
bapt2 <5711@holbertonstudents.com>
2023-02-01 02:43:50 -0800

    test for correct output

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -5,13 +5,12 @@
 def matrix_divided(matrix, div):
     """ divide a matrix """
     n_m = []
+    if type(matrix) is not int:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if type(div) is not int or type(div) is not float:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
     for i in range(len(matrix)):
-        if type(matrix) is not int:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-        if type(div) is not int or type(div) is not float:
-            raise TypeError("div must be a number")
-        if div == 0:
-            raise ZeroDivisionError("division by zero")
-
-            n_m.append(list(map((lambda x: round(x / div, 2)), matrix[i])))
+        n_m.append(list(map((lambda x: round(x / div, 2)), matrix[i])))
     return n_m
