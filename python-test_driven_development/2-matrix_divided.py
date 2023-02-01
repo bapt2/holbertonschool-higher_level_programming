486200499d02f52415482228437f131648e6c02d
bapt2 <5711@holbertonstudents.com>
2023-02-01 04:17:39 -0800

    test 4

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -7,13 +7,22 @@ def matrix_divided(matrix, div):
     n_m = [[]]
     te = TypeError("matrix must be a matrix (list of lists)\
  of integers/floats")
-    
-    if type(div) is not int or type(div) is not float:
-        raise TypeError("div must be a number")
-    if len(matrix) is not int or len(matrix) is not float:
+    if len(matrix) == 0 or len(matrix[0]) == 0:
         raise te
+    if not isinstance(matrix, list):
+        raise te
+    for n_m in matrix:
+        if not isinstance(n_m, list):
+            raise te
+    for n_m in matrix:
+        for i in n_m:
+            if not isinstance(i, int) and not isinstance(i, float):
+                raise te
     if div == 0:
         raise ZeroDivisionError("division by zero")
+    if type(div) is not int or type(div) is not float:
+        raise TypeError("div must be a number")
+
     for i in range(len(matrix)):
         n_m.append(list(map((lambda x: round(x / div, 2)), matrix[i])))
     return n_m
