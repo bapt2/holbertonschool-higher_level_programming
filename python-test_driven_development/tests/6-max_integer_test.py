815d039c37454e1806db3869128d3467c5e44e7b
bapt2 <5711@holbertonstudents.com>
2023-02-02 01:13:36 -0800

    added file test

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    def test_file_0(self):
+        self.equal_to_0(max_integer(0)):
