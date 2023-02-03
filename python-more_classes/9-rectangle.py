f27a442ca760c8ade85bc9989dfb6c468e32c931
bapt2 <5711@holbertonstudents.com>
2023-02-03 03:44:14 -0800

    try to fix a syntax error

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -44,7 +44,7 @@ class Rectangle:
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__height = value
 
     def __str__(self):
