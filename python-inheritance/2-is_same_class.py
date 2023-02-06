4ef26e50e253b3f7bb058571a09127580a23fc0f
bapt2 <5711@holbertonstudents.com>
2023-02-06 12:04:57 -0800

    function like isinstance

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" function like isinstance """
+
+
 def is_same_class(obj, a_class):
+    """ function like isinstance """
     if type(obj) is a_class:
         return True
     else:
