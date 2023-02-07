4980ebe53556ccbb1e001561270e1ca71f876691
bapt2 <5711@holbertonstudents.com>
2023-02-07 01:06:22 -0800

    added some doc

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""  """
+""" class that inherited (directly or indirectly) """
 
 
 def inherits_from(obj, a_class):
-    """  """
+    """ class that inherited (directly or indirectly) """
     if type(obj) is not a_class and isinstance(obj, a_class):
         return True
     else:
