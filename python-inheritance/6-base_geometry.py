7c5ed228a48bc043d0576a081fdad04baf1fa624
bapt2 <5711@holbertonstudents.com>
2023-02-07 01:27:27 -0800

    added some doc

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""  """
+""" area() is not implemented """
 
 
 class BaseGeometry:
-    """  """
+    """ area() is not implemented """
     def area(self):
         raise Exception("area() is not implemented")
