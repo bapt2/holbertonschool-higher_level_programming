729784208986663d97897f0b3c8279d1a593ea8c
bapt2 <5711@holbertonstudents.com>
2023-02-07 08:12:14 -0800

    added some doc

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""  """
+""" area method """
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """ area method """
     def __init__(self, width, height):
         self.__width = width
         self.__height = height
