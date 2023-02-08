b2b4cd52ee523e940e022acdd4bc2c581394a09e
bapt2 <5711@holbertonstudents.com>
2023-02-08 05:56:09 -0800

    added doc

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+""" doc """
 
 
 import json
 
 
 def save_to_json_file(my_obj, filename):
+    """ doc """
     with open(filename, 'w') as f:
         return json.dump(my_obj, f)
