18808c1428beb614080be7a48891d7cdacf050ae
bapt2 <5711@holbertonstudents.com>
2023-02-08 03:15:13 -0800

    test

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,5 +5,7 @@
 def read_file(filename=""):
     """ doc """
     with open(filename, 'r') as f:
+        if filename == "":
+            pass
         f = f.read()
-        print(f)
+        print(f[:-1])
