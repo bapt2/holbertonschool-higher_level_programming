316247baf94d16dff1c43420e3dda4d26e50d77f
bapt2 <5711@holbertonstudents.com>
2023-02-08 07:25:50 -0800

    test

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,7 +6,7 @@ import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-args =sys.argv
+args = sys.argv
 filename = "add_item.json"
 with open(filename, 'a+') as f:
     my_list = []
