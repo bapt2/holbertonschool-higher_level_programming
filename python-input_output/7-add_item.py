27c81229de6ec89ced949cef8b4ca05edeb9bf50
bapt2 <5711@holbertonstudents.com>
2023-02-08 08:15:15 -0800

    Load, add, save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,10 +6,13 @@ import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-args = sys.argv
-filename = "add_item.json"
-with open(filename, 'a+') as f:
-    my_list = []
-    my_list.extend(args[1:])
-    save_to_json_file(my_list, filename)
-    load_from_json_file(filename)
+if __name__ == '__main__':
+    args = sys.argv
+    filename = "add_item.json"
+    with open(filename, 'r') as f:
+        try:
+            my_list = load_from_json_file(filename)
+        except:
+            my_list = []
+        my_list.extend(args[1:])
+        save_to_json_file(my_list, filename)
