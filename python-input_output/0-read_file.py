ad0d28c185bedf9675e8ef5e73504680a40c9482
bapt2 <5711@holbertonstudents.com>
2023-02-08 03:29:48 -0800

    fix none case

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,7 +5,7 @@
 def read_file(filename=""):
     """ doc """
     with open(filename, 'r') as f:
-        if filename == "":
-            pass
         f = f.read()
+        if f == "":
+            return
         print(f[:-1])
