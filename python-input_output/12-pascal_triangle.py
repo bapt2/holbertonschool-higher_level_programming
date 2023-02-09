5cbead7dac404669326832bd7ad0893309c658e4
bapt2 <5711@holbertonstudents.com>
2023-02-09 05:58:38 -0800

    added pascal triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" pascal triangle """
+
+
+def pascal_triangle(n):
+    """ pascal triangle """
+    if n <= 0:
+        return []
+
+    a = []
+    b = []
+    for i in range(n):
+        if i == 0:
+            a.append([1])
+        else:
+            b = [1]
+        for j in range(i - 1):
+            b.append(a[i - 1][j] + a[i - 1][j + 1])
+        b.append(1)
+        a.append(b)
+    return a
