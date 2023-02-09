f7971b830afb2b2ec061ef7404ce85d568f70c66
bapt2 <5711@holbertonstudents.com>
2023-02-09 02:42:55 -0800

    test

---
+++ python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" class Student """
+
+
+class Student:
+    """ """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list):
+            nl = {}
+            for key, value in self.__dict__.items():
+                for element in attrs:
+                    if key == element:
+                        nl[key] = value
+            return nl
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
