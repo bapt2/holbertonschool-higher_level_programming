5d1d2ffb8e6ec37b30e3323f800071ca707130b4
bapt2 <5711@holbertonstudents.com>
2023-02-14 06:45:46 -0800

    added to_dictionary method

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -37,3 +37,12 @@ class Square(Rectangle):
                 self.y = args[3]
         for key, value in kwargs.items():
             setattr(self, key, value)
+
+    def to_dictionary(self):
+        """ dictionary """
+        return {
+            "id": self.id,
+            "size": self.size,
+            "x": self.x,
+            "y": self.y
+            }
