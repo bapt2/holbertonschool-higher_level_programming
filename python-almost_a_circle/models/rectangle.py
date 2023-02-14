42ed55f63c6bd3e66dfce701690fdac1d35c2e83
bapt2 <5711@holbertonstudents.com>
2023-02-14 02:41:47 -0800

    update the update method

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -82,7 +82,7 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}"\
             .format(self.id, self.x, self.y, self.width, self.height)
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """ assigns an argument to each attribute """
         if len(args) != 0:
             if len(args) >= 1:
@@ -95,3 +95,5 @@ class Rectangle(Base):
                 self.x = args[3]
             if len(args) > 4:
                 self.y = args[4]
+        for key, value in kwargs.items():
+            setattr(self, key, value)
