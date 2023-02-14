59a7d7764e8f029018c53a1bd2677e5d338e681f
bapt2 <5711@holbertonstudents.com>
2023-02-14 06:40:51 -0800

    added to_dictionary method

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -97,3 +97,13 @@ class Rectangle(Base):
                 self.y = args[4]
         for key, value in kwargs.items():
             setattr(self, key, value)
+
+    def to_dictionary(self):
+        """ dictionary """
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y,
+            }
