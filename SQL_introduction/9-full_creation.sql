496c91f38bc9bcc6c505a0a515fd03de143c6a0a
bapt2 <5711@holbertonstudents.com>
2023-03-14 09:37:55 +0100

    fixing the second table

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,3 +1,6 @@
 -- create a table second_table
 CREATE TABLE IF NOT EXISTS second_table (id int, name VARCHAR(256), score int);
-INSERT INTO second_table (id, name, score) VALUES(1, 2, 3, 4), ("Jhon", "Alex", "Bob", "George"), (10, 3, 14, 8);
+INSERT INTO second_table VALUES(1, "Jhon", 10);
+INSERT INTO second_table VALUES(2, "Alex", 3);
+INSERT INTO second_table VALUES(3, "Bob", 14);
+INSERT INTO second_table VALUES(4, "George", 8);
