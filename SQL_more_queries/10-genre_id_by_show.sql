e0aee54a41d601fdccc75fb7c1d15e5d520b5240
bapt2 <5711@holbertonstudents.com>
2023-03-15 10:24:31 +0100

    fixing issue

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
--- inport the database dump from hbtn_0d_tvshows to your mysql server
-SELECT tv_shows.title, tv_shows_genre.genre_id
-FROM tv_shows, tv_shows_genre
-WHERE tv_shows.id = tv_shows_genre.show_id
-ORDER BY tv_shows.title ASC, tv_shows_genre.genre_id;
+-- inport the database dump from hbtn_0d_tvshow to your mysql server
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows, tv_show_genres
+WHERE tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id;
