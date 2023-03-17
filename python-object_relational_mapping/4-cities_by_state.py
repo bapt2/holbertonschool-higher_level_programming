0a436692c942e6d0255a62dec9e2e1ace8afe458
bapt2 <5711@holbertonstudents.com>
2023-03-17 16:00:33 +0100

    trying to fix an issue

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -10,14 +10,13 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    statename = sys.argv[4]
     connection = MySQLdb.connect(user=username, passwd=password,
                                  db=database, host="localhost",
                                  port=3306)
 
     cursor = connection.cursor()
-    request = "SELECT * FROM states WHERE name = %s ORDER BY states.id"
-    cursor.execute(request, (statename,))
+    request = "SELECT * FROM states WHERE state ORDER BY cities.id"
+    cursor.execute(request)
 
     stateList = cursor.fetchall()
 
