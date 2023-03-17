f1664bada9abba8e83ab35574d8fd995362fc0e0
bapt2 <5711@holbertonstudents.com>
2023-03-17 14:36:02 +0100

    Filter states by user input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
                                  port=3306)
 
     cursor = connection.cursor()
-    request = "SELECT * FROM states '{}'\
+    request = "SELECT * FROM states WHERE name LIKE ID'{}'\
         ORDER BY states.id".format(statename)
     cursor.execute(request)
 
