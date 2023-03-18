3d69be709508eeb39cf724b397345a864c1f9a25
bapt2 <5711@holbertonstudents.com>
2023-03-18 14:31:56 +0100

    fixing pycodestyle

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-""" python class that contains the class definition of a State and an instance Base = declarative_base()"""
+""" python class that contains the class definition
+    of a State and an instance Base = declarative_base()
+"""
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+
 
 Base = declarative_base()
 
