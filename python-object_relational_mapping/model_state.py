fca892e93a9edef7b139ab24e10b16e193acb94d
bapt2 <5711@holbertonstudents.com>
2023-03-18 14:29:51 +0100

    adding comment in the class

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -7,6 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class State(Base):
+    """ State class"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, unique=True, nullable=False)
     name = Column(String(128), nullable=False)
