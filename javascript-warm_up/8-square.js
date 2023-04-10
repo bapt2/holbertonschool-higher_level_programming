85cced58a221db2567faf9ba760aedba269c44ba
bapt2 <5711@holbertonstudents.com>
2023-04-10 19:06:22 +0200

    fixing correct output

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,15 +1,10 @@
 #!/usr/bin/node
 
 const x = parseInt(process.argv[2], 10);
-let X = '';
 if (x) {
   for (let i = 0; i < x; i++) {
-    for (let j = 0; j < x; j++) {
-      X += 'X';
-    }
-    X += '\n';
+    console.log('X'.repeat(x));
   }
-  console.log(X);
 } else {
   console.log('Missing size');
 }
