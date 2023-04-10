1407c0499b56b13c9e263ed244b40376b12eaafe
bapt2 <5711@holbertonstudents.com>
2023-04-10 19:37:00 +0200

    fixing semistandard

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-function add(a, b) {
+function add (a, b) {
   a = parseInt(process.argv[2], 10);
   b = parseInt(process.argv[3], 10);
-  console.log(a + b)
+  console.log(a + b);
 }
 add(process.argv[2], process.argv[3]);
