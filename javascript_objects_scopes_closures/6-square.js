f2aaa606798ecbd893cd4ea125cafb56b3a48906
bapt2 <5711@holbertonstudents.com>
2023-04-11 12:42:35 +0200

    adding an instance method charPrint

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const Rectangle = require('./5-square');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    for (let i = 0; i < this.width; i++) {
+      if (c === undefined) {
+        console.log('X'.repeat(this.height));
+      } else {
+        console.log('C'.repeat(this.height));
+      }
+    }
+  }
+}
+module.exports = Square;
