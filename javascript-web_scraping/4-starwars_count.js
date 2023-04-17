c56cb197f25101b88d241a69c5faf856538f7ed0
bapt2 <5711@holbertonstudents.com>
2023-04-17 15:24:10 +0200

    come back to my code

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,23 +1,21 @@
 #!/usr/bin/node
 
 const request = require('request');
-let count = 0;
-const url = String(process.argv[2]);
-const characterId = '18';
+const id = process.argv[2];
 
-request({
-  url: url,
-  json: true
-}, (err, response, body) => {
-  if (err) {
-    console.log('Error:', err);
-  } for (const i in body.results) {
-    for (const j of body.results[i].characters) {
-      if (j === `https://swapi-api.hbtn.io/api/people/${characterId}/`) {
-        count += 1;
+request(id, (err, responce, body) => {
+  if (!err) {
+    const Body = JSON.parse(body);
+    let count = 0;
+    const result = Body.result;
+    for (let i = 0; i < result.length; i++) {
+      const characters = result[i].characters;
+      for (let j = 0; j < characters.length; j++) {
+        if (characters[j].search('18') > 0) {
+          count += 1;
+        }
       }
     }
+    console.log(count);
   }
-  console.log(count);
-}
-);
+});
