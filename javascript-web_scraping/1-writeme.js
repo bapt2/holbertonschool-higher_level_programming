a779221f6ebcbfcf5fa9587f7a9b2ee9f1b9670c
bapt2 <5711@holbertonstudents.com>
2023-04-17 10:16:14 +0200

    adding a script that write a string to a file

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const argv = process.argv[2];
+const data = process.argv[3];
+
+fs.writeFile(argv, data, 'utf-8', (err, data) => {
+  if (err) {
+    return;
+  }
+});
