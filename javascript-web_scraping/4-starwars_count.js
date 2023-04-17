4d0a5c353bac1eaecc7cc0fdd413b3f7fb3f9b42
bapt2 <5711@holbertonstudents.com>
2023-04-17 15:07:56 +0200

    testing something

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -7,9 +7,13 @@ request(id, (err, responce, body) => {
   if (!err) {
     const Body = JSON.parse(body);
     let count = 0;
-    for (const i in Body.characters) {
-      if (i === 'https://swapi-api.hbtn.io/api/people/18/') {
-        count += 1;
+    const result = Body.result;
+    for (let i = 0; i < result.length; i++) {
+      const characters = result[i].characters;
+      for (let j = 0; j < characters.length; j++) {
+        if (characters[j].search('18') > 0) {
+          count += 1;
+        }
       }
     }
     console.log(count);
