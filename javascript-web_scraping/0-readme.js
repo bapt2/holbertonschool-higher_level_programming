1be16da46212d2f5485133b82731219131d0bfdd
bapt2 <5711@holbertonstudents.com>
2023-04-17 10:00:27 +0200

    fixing semistandard

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const argv = process.argv.slice(2)
+const argv = process.argv.slice(2);
 
 fs.readFile(String(argv), 'utf-8', (err, data) => {
   if (err) {
