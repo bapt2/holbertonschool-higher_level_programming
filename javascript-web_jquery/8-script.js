3cbef8597877986637f53e27287a1a83ca2b7b8c
bapt2 <5711@holbertonstudents.com>
2023-04-18 09:03:14 +0200

    script that add each title of an url

---
+++ javascript-web_jquery/8-script.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+$.get("https://swapi-api.hbtn.io/api/films/?format=json", function (data) {
+  $.each(data.results, function (key, value) {
+    $('UL#list_movies').append(`<li>${value.title}</li>`);
+  });
+});
